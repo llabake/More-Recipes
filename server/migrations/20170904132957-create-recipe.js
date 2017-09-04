8a8535885c1fdeb4436b691e6451f20a6c341fdf
llabake <lemboyelabake@gmail.com>
2017-09-04 16:04:10 +0100

    Add model and migration for recipe

---
+++ server/migrations/20170904132957-create-recipe.js
@@ -0,0 +1,40 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Recipes', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    title: {
+      type: Sequelize.STRING
+    },
+    direction: {
+      type: Sequelize.TEXT
+    },
+    ingredients: {
+      type: Sequelize.TEXT
+    },
+    image: {
+      type: Sequelize.STRING
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    userId: {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'Users',
+        key: 'id',
+        as: 'userId',
+      },
+      onDelete: 'CASCADE'
+    }
+  }),
+  down: queryInterface => queryInterface.dropTable('Recipes')
+};
---
+++ server/models/recipe.js
@@ -0,0 +1,46 @@
+module.exports = (sequelize, DataTypes) => {
+  const Recipe = sequelize.define('Recipe', {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: {
+          args: [1, 25],
+          msg: 'Minimum of 1 character and Maximum of 50 characters required'
+        }
+      }
+    },
+    direction: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: {
+          args: [1, 1000],
+          msg: 'Minimum of 1 character and Maximum of 1000 characters required'
+        }
+      }
+    },
+    ingredients: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: {
+          args: [1, 500],
+          msg: 'Minimum of 1 character and Maximum of 500 characters required'
+        }
+      }
+    },
+    image: {
+      type: DataTypes.STRING
+    }
+  }, {
+    classMethods: {
+      associate: (models) => {
+        Recipe.belongsTo(models.User, {
+          foreignKey: 'userId'
+        });
+      }
+    }
+  });
+  return Recipe;
+};
