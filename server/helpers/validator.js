7284a748b706869708a42c58f2b8b88f663189e5
llabake <lemboyelabake@gmail.com>
2017-09-09 11:46:37 +0100

    refactor validator logic

--- server/helpers/validator.js
+++ server/helpers/validator.js
@@ -1,24 +1,12 @@
 export const signUpValidator = (req, res, next) => {
   const errors = [];
-  if (!req.body.username) {
-    errors.push({ path: 'username', message: 'username is required' });
-  }
-  if (!req.body.firstName) {
-    errors.push({ path: 'firstName', message: 'firstName is required' });
-  }
-  if (!req.body.lastName) {
-    errors.push({ path: 'lastName', message: 'lastName is required' });
-  }
-  if (!req.body.email) {
-    errors.push({ path: 'email', message: 'email is required' });
-  }
-  if (!req.body.password) {
-    errors.push({ path: 'password', message: 'password is required' });
-  }
-  if (!req.body.confirmpassword) {
-    errors.push({ path: 'confirmpassword', message: 'confirmpassword is required' });
-  }
-  if (req.body.password != req.body.confirmpassword) {
+  const requiredFields = ['username', 'firstName', 'lastName', 'email', 'password', 'confirmpassword']
+  requiredFields.forEach((field) => {
+    if (!req.body[field]) {
+      errors.push({ path: field, message: `${field} is required` });
+    }
+  })
+  if (req.body.password !== req.body.confirmpassword) {
     errors.push({ path: 'password', message: 'Please ensure the Password match' });
   }
   if (errors.length === 0) {
@@ -30,12 +18,12 @@ export const signUpValidator = (req, res, next) => {
 
 export const signInValidator = (req, res, next) => {
   const errors = [];
-  if (!req.body.username) {
-    errors.push({ path: 'username', message: 'username is required' });
-  }
-  if (!req.body.password) {
-    errors.push({ path: 'password', message: 'password is required' });
-  }
+  const requiredFields = ['username', 'password'];
+  requiredFields.forEach((field) => {
+    if (!req.body[field]) {
+      errors.push({ path: field, message: `${field} is required` });
+    }
+  })
   if (errors.length === 0) {
     next();
   } else {
